6a1f69ae0e0599a70e3440cd7ed50e61d4bf4ce2
opa <opa@41ad046d-2ef8-41eb-9ac2-fc19ede17269>
2009-08-15 06:59:06 +0000

    ・新規作成
    git-svn-id: https://www.opa.to/svn/prj/true/trunk@129 41ad046d-2ef8-41eb-9ac2-fc19ede17269

---
+++ _setup.bat
@@ -0,0 +1,4 @@
+call AddPath "%~d0\opt\bcc55\bin"
+set INCLUDE=%~d0\opt\bcc55\include
+set DIRCMD=/a /ogn
+: doskey make=make -f MAKEFILE.borland $*
---
+++ true.c
@@ -0,0 +1,20 @@
+/*
+	true
+
+	by opa
+*/
+
+#define PGM					"true"
+#define PGM_DEBUG			PGM ": "
+#define PGM_INFO			PGM ": "
+#define PGM_WARN			PGM " warning: "
+#define PGM_ERR				PGM " error: "
+#define VERSTR				"1.00"
+
+#define CREDIT2009			"Copyright (c) 2009 by opa"
+
+char
+	credit[] = PGM " version " VERSTR " " CREDIT2009;
+
+int wmain() { return 0; }
+
